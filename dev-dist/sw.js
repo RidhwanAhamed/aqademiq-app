9b4b27c278e9ce9315e4a25edff5f8c6fc4728cd
Mohammed-Aswath <mohammed.aswath07@gmail.com>
2025-11-22 22:15:31 +0530

    Debugged calendar time bug

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.dv3gl58md8g"
+    "revision": "0.h7tq4ua3bc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
